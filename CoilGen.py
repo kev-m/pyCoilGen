57df15dc119f5cacb567b1c8017b49df620b6f7b
Kevin Meyer <kev-m@users.noreply.github.com>
2023-05-31 15:02:50 +0200

    Adding more files

---
+++ CoilGen.py
@@ -0,0 +1,179 @@
+import logging
+
+import sys
+from pathlib import Path
+
+# Add the sub_functions directory to the Python module search path
+sub_functions_path = Path(__file__).resolve().parent / 'sub_functions'
+sys.path.append(str(sub_functions_path))
+
+# Import the required modules from sub_functions directory
+from parse_input import parse_input
+from read_mesh import read_mesh
+
+"""
+from split_disconnected_mesh import split_disconnected_mesh
+from refine_mesh import refine_mesh
+from parameterize_mesh import parameterize_mesh
+from define_target_field import define_target_field
+from temp_evaluation import temp_evaluation
+from calculate_one_ring_by_mesh import calculate_one_ring_by_mesh
+from calculate_basis_functions import calculate_basis_functions
+from calculate_sensitivity_matrix import calculate_sensitivity_matrix
+from calculate_gradient_sensitivity_matrix import calculate_gradient_sensitivity_matrix
+from calculate_resistance_matrix import calculate_resistance_matrix
+from stream_function_optimization import stream_function_optimization
+from calc_potential_levels import calc_potential_levels
+from calc_contours_by_triangular_potential_cuts import calc_contours_by_triangular_potential_cuts
+from process_raw_loops import process_raw_loops
+from find_minimal_contour_distance import find_minimal_contour_distance
+from topological_loop_grouping import topological_loop_grouping
+from calculate_group_centers import calculate_group_centers
+from interconnect_within_groups import interconnect_within_groups
+from interconnect_among_groups import interconnect_among_groups
+from shift_return_paths import shift_return_paths
+from generate_cylindrical_pcb_print import generate_cylindrical_pcb_print
+from create_sweep_along_surface import create_sweep_along_surface
+from calculate_inductance_by_coil_layout import calculate_inductance_by_coil_layout
+from evaluate_field_errors import evaluate_field_errors
+from calculate_gradient import calculate_gradient
+from load_preoptimized_data import load_preoptimized_data
+"""
+
+def CoilGen(log):
+    # Create optimized coil finished coil layout
+    # Autor: Philipp Amrein, University Freiburg, Medical Center, Radiology, Medical Physics
+    # 5.10.2021
+
+    # the following external functions were used in modified form:
+    # intreparc@John D'Errico (2010), @matlabcentral/fileexchange
+    # The non-cylindrical parameterization is taken from "matlabmesh @ Ryan
+    # Schmidt rms@dgp.toronto.edu" based on desbrun et al (2002), "Intrinsic Parameterizations of {Surface} Meshes",
+    # NS (2021). Curve intersections (https://www.mathworks.com/matlabcentral/fileexchange/22441-curve-intersections), MATLAB Central File Exchange.
+
+    # Parse the input variables
+    input_parser, input_args = parse_input(arg_list=None)
+
+    # Print the input variables
+    log.debug('Parse inputs: %s', input_args)
+
+    if input_args.sf_source_file == 'none':
+        # Read the input mesh
+        print('Load geometry:')
+        coil_mesh, target_mesh, secondary_target_mesh = read_mesh(input_args)
+
+        # Split the mesh and the stream function into disconnected pieces
+        print('Split the mesh and the stream function into disconnected pieces.')
+        coil_parts = split_disconnected_mesh(coil_mesh)
+
+        # Upsample the mesh density by subdivision
+        print('Upsample the mesh by subdivision:')
+        coil_parts = refine_mesh(coil_parts, input_args)
+
+        # Parameterize the mesh
+        print('Parameterize the mesh:')
+        coil_parts = parameterize_mesh(coil_parts, input_args)
+
+        # Define the target field
+        print('Define the target field:')
+        target_field, is_supressed_point = define_target_field(coil_parts, target_mesh, secondary_target_mesh, input_args)
+
+        # Evaluate the temp data; check whether precalculated values can be used from previous iterations
+        print('Evaluate the temp data:')
+        input_args = temp_evaluation(input_args, target_field)
+
+        # Find indices of mesh nodes for one ring basis functions
+        print('Calculate mesh one ring:')
+        coil_parts = calculate_one_ring_by_mesh(coil_parts, input_args)
+
+        # Create the basis function container which represents the current density
+        print('Create the basis function container which represents the current density:')
+        coil_parts = calculate_basis_functions(coil_parts, input_args)
+
+        # Calculate the sensitivity matrix Cn
+        print('Calculate the sensitivity matrix:')
+        coil_parts = calculate_sensitivity_matrix(coil_parts, target_field, input_args)
+
+        # Calculate the gradient sensitivity matrix Gn
+        print('Calculate the gradient sensitivity matrix:')
+        coil_parts = calculate_gradient_sensitivity_matrix(coil_parts, target_field, input_args)
+
+        # Calculate the resistance matrix Rmn
+        print('Calculate the resistance matrix:')
+        coil_parts = calculate_resistance_matrix(coil_parts, input_args)
+
+        # Optimize the stream function toward target field and further constraints
+        print('Optimize the stream function toward target field and secondary constraints:')
+        coil_parts, combined_mesh, sf_b_field = stream_function_optimization(coil_parts, target_field, input_args)
+
+    else:
+        # Load the preoptimized data
+        print('Load preoptimized data:')
+        coil_parts, _, _, combined_mesh, sf_b_field, target_field, is_supressed_point = load_preoptimized_data(input_args)
+
+    # Calculate the potential levels for the discretization
+    print('Calculate the potential levels for the discretization:')
+    coil_parts, primary_surface_ind = calc_potential_levels(coil_parts, combined_mesh, input_args)
+
+    # Generate the contours
+    print('Generate the contours:')
+    coil_parts = calc_contours_by_triangular_potential_cuts(coil_parts)
+
+    # Process contours
+    print('Process contours: Evaluate loop significance')
+    coil_parts = process_raw_loops(coil_parts, input_args, target_field)
+
+    if not input_args['skip_postprocessing']:
+        # Find the minimal distance between the contour lines
+        print('Find the minimal distance between the contour lines:')
+        coil_parts = find_minimal_contour_distance(coil_parts, input_args)
+
+        # Group the contour loops in topological order
+        print('Group the contour loops in topological order:')
+        coil_parts = topological_loop_grouping(coil_parts, input_args)
+
+        # Calculate center locations of groups
+        print('Calculate center locations of groups:')
+        coil_parts = calculate_group_centers(coil_parts)
+
+        # Interconnect the single groups
+        print('Interconnect the single groups:')
+        coil_parts = interconnect_within_groups(coil_parts, input_args)
+
+        # Interconnect the groups to a single wire path
+        print('Interconnect the groups to a single wire path:')
+        coil_parts = interconnect_among_groups(coil_parts, input_args)
+
+        # Connect the groups and shift the return paths over the surface
+        print('Shift the return paths over the surface:')
+        coil_parts = shift_return_paths(coil_parts, input_args)
+
+        # Create Cylindrical PCB Print
+        print('Create PCB Print:')
+        coil_parts = generate_cylindrical_pcb_print(coil_parts, input_args)
+
+        # Create Sweep Along Surface
+        print('Create sweep along surface:')
+        coil_parts = create_sweep_along_surface(coil_parts, input_args)
+
+    # Calculate the inductance by coil layout
+    print('Calculate the inductance by coil layout:')
+    coil_inductance, radial_lumped_inductance, axial_lumped_inductance, radial_sc_inductance, axial_sc_inductance = calculate_inductance_by_coil_layout(coil_parts, input_args)
+
+    # Evaluate the field errors
+    print('Evaluate the field errors:')
+    field_errors, _, _ = evaluate_field_errors(coil_parts, target_field, input_args)
+
+    # Calculate the gradient
+    print('Calculate the gradient:')
+    coil_gradient = calculate_gradient(coil_parts, target_field, input_args)
+
+    return coil_parts, combined_mesh, sf_b_field, target_field, coil_inductance, radial_lumped_inductance, axial_lumped_inductance, radial_sc_inductance, axial_sc_inductance, field_errors, coil_gradient, is_supressed_point
+
+if __name__ == "__main__":
+    # Set up logging
+    log = logging.getLogger(__name__)
+    #logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
+
+    CoilGen(log)
---
+++ sub_functions/parse_input.py
@@ -0,0 +1,296 @@
+import argparse
+from pathlib import Path
+
+
+def parse_input(arg_list=None):
+    """
+    Parse the input arguments using argparse.
+
+    Returns:
+    input (argparse.Namespace): Parsed input arguments.
+    """
+
+    # Create argument parser
+    parser = argparse.ArgumentParser()
+
+    # Add the mesh file that represents the boundary of the target geometry
+    parser.add_argument('--temp', type=str, default=None,
+                        help="Mesh file representing the boundary of the target geometry")
+
+    # Add the coil mesh file
+    parser.add_argument('--coil_mesh_file', type=str,
+                        default='none', help="File of the coil mesh")
+
+    # Add the field shape function
+    parser.add_argument('--field_shape_function', type=str,
+                        default='x', help="Spatial function that defines the field")
+
+    # Add the target gradient strength
+    parser.add_argument('--target_gradient_strength', type=float,
+                        default=1, help="Target gradient strength in T/m")
+
+    # Add the offset factor for contour levels
+    parser.add_argument('--pot_offset_factor', type=float,
+                        default=1/2, help="Offset factor for contour levels")
+
+    # Add the target mesh file
+    parser.add_argument('--target_mesh_file', type=str,
+                        default='none', help="File of the target surface mesh")
+
+    # Add the secondary target mesh file
+    parser.add_argument('--secondary_target_mesh_file', type=str,
+                        default='none', help="File of the secondary target mesh")
+
+    # Add the secondary target weight
+    parser.add_argument('--secondary_target_weight', type=float,
+                        default=1, help="Weight for the secondary target points")
+
+    # Add flag to use only target mesh vertices as target coordinates
+    parser.add_argument('--use_only_target_mesh_verts', action='store_true',
+                        help="Flag to use only target mesh vertices as target coordinates")
+
+    # Add the stream function source file
+    parser.add_argument('--sf_source_file', type=str, default='none',
+                        help="File of the already optimized stream function")
+
+    # Add the target field definition file
+    parser.add_argument('--target_field_definition_file', type=str,
+                        default='none', help="File of the target field definition")
+
+    # Add the target field definition field name
+    parser.add_argument('--target_field_definition_field_name', type=str,
+                        default='none', help="Field name of the target field definition")
+
+    # Add the stream function optimization method
+    parser.add_argument('--sf_opt_method', type=str, default='tikkonov',
+                        help="Stream function optimization method")
+
+    # Add the Tikonov regularization factor
+    parser.add_argument('--tikonov_reg_factor', type=float, default=1,
+                        help="Tikonov regularization factor for the stream function optimization")
+
+    # Add the fmincon parameter list
+    parser.add_argument('--fmincon_parameter', nargs='+', type=float, default=[
+                        500, 10**10, 1.0e-10, 1.0e-10, 1.0e-10], help="Parameters for the iterative optimization with fmincon")
+
+    # Add the number of potential levels
+    parser.add_argument('--levels', type=int, default=10,
+                        help="Number of potential levels")
+
+    # Add the level set method
+    parser.add_argument('--level_set_method', type=str, default='primary',
+                        help="Method for calculating the level sets")
+
+    # Add the field type to evaluate
+    parser.add_argument('--fieldtype_to_evaluate', type=str,
+                        default='field', help="Field type to evaluate")
+
+    # Add flag for cylindrical surface
+    parser.add_argument('--surface_is_cylinder_flag',
+                        action='store_true', help="Flag for cylindrical surface")
+
+    # Add the circular diameter factor for cylinder parameterization
+    parser.add_argument('--circular_diameter_factor', type=float, default=1,
+                        help="Circular diameter factor for cylinder parameterization")
+
+    # Add the width in meter of the opening cut for the interconnection of the loops
+    parser.add_argument('--interconnection_cut_width', type=float, default=0.01,
+                        help="Width in meter of the opening cut for the interconnection of the loops")
+
+    # Add the radius of a spherical target field
+    parser.add_argument('--target_region_radius', type=float,
+                        default=0.15, help="Radius of a spherical target field")
+
+    # Add the number of target points per dimension within the target region
+    parser.add_argument('--target_region_resolution', type=int, default=10,
+                        help="Number of target points per dimension within the target region")
+
+    # Add the distance in meter for which crossing lines will be separated along the normal direction of the surface
+    parser.add_argument('--normal_shift_length', type=float, default=0.001,
+                        help="Distance in meter for which crossing lines will be separated along the normal direction of the surface")
+
+    # Add the minimal required number of point of a single loop; otherwise loops will be removed
+    parser.add_argument('--min_point_loop_number', type=int, default=20,
+                        help="Minimal required number of points of a single loop; otherwise loops will be removed")
+
+    # Add the minimal required field contribution (in percent) to the target field; loops that contribute less than that can be deleted
+    parser.add_argument('--min_loop_significance', type=int, default=1,
+                        help="Minimal required field contribution (in percent) to the target field; loops that contribute less than that can be deleted")
+
+    # Add additional loop removal criteria which relates to the perimeter to surface ratio of the loop
+    parser.add_argument('--area_perimeter_deletion_ratio', type=int, default=5,
+                        help="Additional loop removal criteria which relates to the perimeter to surface ratio of the loop")
+
+    # Add the maximum allowed angle of the track of the contours
+    parser.add_argument('--max_allowed_angle_within_coil_track', type=int,
+                        default=120, help="Maximum allowed angle of the track of the contours")
+
+    # Add the minimum allowed angle of the track of the contours; smaller angles will be converted to straight lines in order to reduce the number of points
+    parser.add_argument('--min_allowed_angle_within_coil_track', type=float, default=0.0001,
+                        help="Minimum allowed angle of the track of the contours; smaller angles will be converted to straight lines in order to reduce the number of points")
+
+    # Add the minimum relative percentage for which points will be deleted which contribute to segments which are extremely short
+    parser.add_argument('--tiny_segment_length_percentage', type=float, default=0,
+                        help="Minimum relative percentage for which points will be deleted which contribute to segments which are extremely short")
+
+    # Add the number of refinement iterations of the mesh together with the stream function
+    parser.add_argument('--iteration_num_mesh_refinement', type=int, default=0,
+                        help="Number of refinement iterations of the mesh together with the stream function")
+
+    # Add the direction (vector) along which the interconnections will be aligned
+    parser.add_argument('--b_0_direction', nargs='+', type=float, default=[
+                        0, 0, 1], help="Direction (vector) along which the interconnections will be aligned")
+
+    # Add the directory of the .stl geometry files
+    parser.add_argument('--geometry_source_path', type=str, default=str(
+        Path.cwd() / 'Geometry_Data'), help="Directory of the .stl geometry files")
+
+    # Add the output directory
+    parser.add_argument('--output_directory', type=str,
+                        default=str(Path.cwd()), help="Output directory")
+
+    # Add flag if the track should be smoothed
+    parser.add_argument('--smooth_flag', action='store_true',
+                        help="Flag if the track should be smoothed")
+
+    # Add the smoothing parameter
+    parser.add_argument('--smooth_factor', type=int,
+                        default=1, help="Smoothing parameter")
+
+    # Add flag to save sweeped .stl
+    parser.add_argument('--save_stl_flag', action='store_true',
+                        help="Flag to save swept .stl")
+
+    # Add flag to plot results
+    parser.add_argument('--plot_flag', action='store_true',
+                        help="Flag to plot results")
+
+    # Add interconnection_method: Regular or spiral in/out
+    parser.add_argument('--interconnection_method', type=str, default='regular',
+                        help="Interconnection method: 'regular' or 'spiral' in/out")
+
+    # Add group_interconnection_method: 'straight' or 'crossed'
+    parser.add_argument('--group_interconnection_method', type=str, default='crossed',
+                        help="Group interconnection method: 'straight' or 'crossed'")
+
+    # Add flag to skip calculation of minimal winding distance
+    parser.add_argument('--skip_calculation_min_winding_distance', action='store_true',
+                        help="Flag to skip calculation of minimal winding distance")
+
+    # Add flag to skip post processing
+    parser.add_argument('--skip_postprocessing',
+                        action='store_true', help="Flag to skip post-processing")
+
+    # Add flag to skip inductance_calculation
+    parser.add_argument('--skip_inductance_calculation',
+                        action='store_true', help="Flag to skip inductance calculation")
+
+    # Flag to skip the shifting of return paths
+    parser.add_argument('--skip_normal_shift', action='store_true',
+                        help="Flag to skip the shifting of return paths")
+
+    # Add smoothing parameters regarding the normal shift
+    parser.add_argument('--normal_shift_smooth_factors', nargs='+', type=int,
+                        default=[2, 3, 2], help="Smoothing parameters regarding the normal shift")
+
+    # Flag to skip the generation of a volumetric (3D) coil body
+    parser.add_argument('--skip_sweep', action='store_true',
+                        help="Flag to skip the generation of a volumetric (3D) coil body")
+
+    # Flag to generate a rectangular pcb pattern to wrap around a cylinder
+    parser.add_argument('--make_cylindrical_pcb', action='store_true',
+                        help="Flag to generate a rectangular pcb pattern to wrap around a cylinder")
+
+    # Add the pcb_interconnection_method: 'spiral_in_out' or other options
+    parser.add_argument('--pcb_interconnection_method', type=str, default='spiral_in_out',
+                        help="Interconnection method for PCB: 'spiral_in_out' or other options")
+
+    # Add the factor of shifting the open ends of the spirals in order to avoid overlaps; in percent
+    parser.add_argument('--pcb_spiral_end_shift_factor', type=int, default=10,
+                        help="Factor of shifting the open ends of the spirals in order to avoid overlaps; in percent")
+
+    # Add force_cut_selection
+    parser.add_argument('--force_cut_selection', nargs='+',
+                        default=[], help="Force cut selection")
+
+    # Add the Gauss integration order
+    parser.add_argument('--gauss_order', type=int, default=2,
+                        help="Gauss integration order")
+
+    # Add flag to set the roi into the geometric center of the mesh
+    parser.add_argument('--set_roi_into_mesh_center', type=int, default=0,
+                        help="Flag to set the ROI into the geometric center of the mesh")
+
+    # In case of pcb layout, specify the track width
+    parser.add_argument('--track_width_factor', type=float,
+                        default=0.5, help="Track width factor for PCB layout")
+
+    # Add the cross_section_width of the conductor (for the inductance calculation) in meter
+    parser.add_argument('--conductor_cross_section_width', type=float, default=0.002,
+                        help="Cross-section width of the conductor (for the inductance calculation) in meter")
+
+    # Add the cross_section_height of the conductor (for the inductance calculation) in meter
+    parser.add_argument('--conductor_cross_section_height', type=float, default=0.002,
+                        help="Cross-section height of the conductor (for the inductance calculation) in meter")
+
+    # Add the conductor conductivity
+    parser.add_argument('--specific_conductivity_conductor',
+                        type=float, default=0.018e-6, help="Conductor conductivity")
+
+    # Add the thickness of the sheet current density within the stream function representation
+    parser.add_argument('--conductor_thickness', type=float, default=0.005,
+                        help="Thickness of the sheet current density within the stream function representation")
+
+    # Add the 2D edge points for direct definition of the cross section of the conductor (build circular cut shapes)
+    parser.add_argument('--cross_sectional_points', nargs='+', type=float, default=[
+                        0, 0], help="2D edge points for direct definition of the cross section of the conductor (build circular cut shapes)")
+
+    # Add the parameters for the generation of the (default) cylindrical mesh
+    parser.add_argument('--cylinder_mesh_parameter_list', nargs='+', type=float, default=[
+                        0.8, 0.3, 20, 20, 1, 0, 0, 0], help="Parameters for the generation of the (default) cylindrical mesh")
+
+    # Add the parameters for the generation of the (default) planar mesh
+    parser.add_argument('--planar_mesh_parameter_list', nargs='+', type=float, default=[
+                        0.25, 0.25, 20, 20, 1, 0, 0, 0, 0, 0, 0], help="Parameters for the generation of the (default) planar mesh")
+
+    # Add the parameters for the generation of a double cone ("diabolo") shaped mesh
+    parser.add_argument('--double_cone_mesh_parameter_list', nargs='+', type=float, default=[
+                        0.8, 0.3, 0.3, 0.1, 20, 20, 1, 0, 0, 0], help="Parameters for the generation of a double cone ('diabolo') shaped mesh")
+
+    # Add the parameters for the generation of the (default) circular mesh
+    parser.add_argument('--circular_mesh_parameter_list', nargs='+', type=float, default=[
+                        0.25, 20, 1, 0, 0, 0, 0, 0, 0], help="Parameters for the generation of the (default) circular mesh")
+
+    # Add the parameters for the generation of the (default) biplanar mesh
+    parser.add_argument('--biplanar_mesh_parameter_list', nargs='+', type=float, default=[
+                        0.25, 0.25, 20, 20, 1, 0, 0, 0, 0, 0, 0.2], help="Parameters for the generation of the (default) biplanar mesh")
+
+    # Parse the input arguments
+    if arg_list is not None:
+        input = parser.parse_args(arg_list)
+    else:
+        input = parser.parse_args()
+
+    return parser, input
+
+
+if __name__ == "__main__":
+    import logging
+    # Set up logging
+    log = logging.getLogger(__name__)
+    logging.basicConfig(level=logging.DEBUG)
+    # logging.basicConfig(level=logging.INFO)
+    
+    # Check that boolean works as expected
+    arg_list = ["--skip_sweep"]
+    parser, input = parse_input(arg_list)
+    log.debug("input.skip_sweep: %s", input.skip_sweep)
+
+    arg_list = []
+    parser, input = parse_input(arg_list)
+    log.debug("input.skip_sweep: %s", input.skip_sweep)
+    
+    # Random other checks
+    log.debug("input.track_width_factor: %s", input.track_width_factor)
+    log.debug("input.cylinder_mesh_parameter_list: %s", input.cylinder_mesh_parameter_list)
+    log.debug("input.coil_mesh_file: %s", input.coil_mesh_file)
