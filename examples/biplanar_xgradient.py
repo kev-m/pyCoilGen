1a55aed79c6c59fd172d819205b63b764882989a
Kevin Meyer <kev-m@users.noreply.github.com>
2023-06-03 13:50:22 +0200

    Adding example

---
+++ examples/biplanar_xgradient.py
@@ -0,0 +1,51 @@
+# System imports
+import sys
+from pathlib import Path
+
+# Logging
+import logging
+
+# Local imports
+# Add the sub_functions directory to the Python module search path
+sub_functions_path = Path(__file__).resolve().parent / '..'
+print(sub_functions_path)
+sys.path.append(str(sub_functions_path))
+
+from CoilGen import CoilGen
+
+if __name__ == "__main__":
+    # Set up logging
+    log = logging.getLogger(__name__)
+    logging.basicConfig(level=logging.DEBUG)
+    # logging.basicConfig(level=logging.INFO)
+
+    arg_list = [
+        '--field_shape_function', 'x',  # definition of the target field
+        '--coil_mesh_file', 'bi_planer_rectangles_width_1000mm_distance_500mm.stl',
+        '--target_mesh_file', 'none',
+        '--secondary_target_mesh_file', 'none',
+        '--secondary_target_weight', '0.5',
+        '--target_region_radius', '0.1',  # in meter
+        # '--use_only_target_mesh_verts', 'False',
+        '--sf_source_file', 'none',
+        # the number of potential steps that determines the later number of windings (Stream function discretization)
+        '--levels', '14',
+        # a potential offset value for the minimal and maximal contour potential ; must be between 0 and 1
+        '--pot_offset_factor', '0.25',
+        '--surface_is_cylinder_flag', 
+        # the width for the interconnections are interconnected; in meter
+        '--interconnection_cut_width', '0.05',
+        # the length for which overlapping return paths will be shifted along the surface normals; in meter
+        '--normal_shift_length', '0.01',
+        '--iteration_num_mesh_refinement', '1',  # the number of refinements for the mesh;
+        '--set_roi_into_mesh_center',
+        '--force_cut_selection', "{'high'}",
+        # Specify one of the three ways the level sets are calculated: "primary","combined", or "independent"
+        '--level_set_method', 'primary',
+        '--interconnection_method', 'regular',
+        #'--skip_postprocessing', 
+        #'--skip_inductance_calculation', 
+        '--tikonov_reg_factor', '10']  # Tikonov regularization factor for the SF optimization
+
+    coil_parts, combined_mesh, sf_b_field, target_field, coil_inductance, radial_lumped_inductance, axial_lumped_inductance, radial_sc_inductance, axial_sc_inductance, field_errors, coil_gradient, is_supressed_point = CoilGen(
+        log, arg_list)
