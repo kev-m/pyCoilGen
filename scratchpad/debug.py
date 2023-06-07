5caad0f01cf88e669fd3963474906a8b9fbff536
Kevin Meyer <kev-m@users.noreply.github.com>
2023-06-07 16:21:53 +0200

    Adding testable mesh

--- scratchpad/debug.py
+++ scratchpad/debug.py
@@ -1,6 +1,7 @@
 # System imports
 import sys
 from pathlib import Path
+import numpy as np
 
 # Logging
 import logging
@@ -14,11 +15,7 @@ sys.path.append(str(sub_functions_path))
 # Do not move import from here!
 from CoilGen import CoilGen
 
-if __name__ == "__main__":
-    # Set up logging
-    log = logging.getLogger(__name__)
-    logging.basicConfig(level=logging.DEBUG)
-
+def debug1():
     # planar_mesh_parameter_list
     planar_height = 1.0
     planar_width = 0.5
@@ -81,3 +78,79 @@ if __name__ == "__main__":
 
     coil_parts, combined_mesh, sf_b_field, target_field, coil_inductance, radial_lumped_inductance, axial_lumped_inductance, radial_sc_inductance, axial_sc_inductance, field_errors, coil_gradient, is_supressed_point = CoilGen(
         log, arg_dict)
+
+
+def debug2():
+    from sub_functions.data_structures import DataStructure, Mesh
+    from sub_functions.parameterize_mesh import parameterize_mesh
+
+    input_params = DataStructure(
+        surface_is_cylinder_flag=False, circular_diameter_factor=0.0)
+
+    # Create small planar mesh with a hole
+    # Define the mesh parameters
+    x_min, x_max = -1.0, 1.0
+    y_min, y_max = -1.0, 1.0
+    hole_min, hole_max = -0.25, 0.25
+    num_rows, num_cols = 8, 8
+
+    # Calculate step sizes
+    x_step = (x_max - x_min) / num_rows
+    y_step = (y_max - y_min) / num_cols
+
+    # Generate vertices
+    vertices = []
+    for i in range(num_rows + 1):
+        for j in range(num_cols + 1):
+            x = x_min + i * x_step
+            y = y_min + j * y_step
+
+            # Check if the vertex is inside the hole region
+            #if hole_min <= x <= hole_max and hole_min <= y <= hole_max:
+            #    continue  # Skip this vertex
+
+            z = 0.0  # Z-coordinate is 0 for a planar mesh
+            vertices.append([x, y, z])
+
+    vertices = np.array(vertices)
+
+    # Generate faces
+    faces = []
+    for i in range(num_rows):
+        for j in range(num_cols):
+            v1 = i * (num_cols + 1) + j
+            v2 = v1 + 1
+            v3 = v1 + num_cols + 2
+            v4 = v1 + num_cols + 1
+
+            # Check if any vertex is inside the hole region
+            if (hole_min <= vertices[v1, 0] <= hole_max and hole_min <= vertices[v1, 1] <= hole_max) or \
+                    (hole_min <= vertices[v2, 0] <= hole_max and hole_min <= vertices[v2, 1] <= hole_max) or \
+                    (hole_min <= vertices[v3, 0] <= hole_max and hole_min <= vertices[v3, 1] <= hole_max) or \
+                    (hole_min <= vertices[v4, 0] <= hole_max and hole_min <= vertices[v4, 1] <= hole_max):
+                continue  # Skip this face
+
+            faces.append([v3, v2, v1])
+            faces.append([v4, v3, v1])
+
+    faces = np.array(faces)
+    # Print the vertices and faces arrays
+    print("Vertices:\n", vertices)
+    print("Faces:\n", faces)
+
+    mesh = Mesh(vertices=vertices, faces=faces)
+
+    mesh.display()
+
+    from sub_functions.data_structures import DataStructure
+    parts = [DataStructure(coil_mesh=mesh)]
+    result = parameterize_mesh(parts, input_params)
+
+
+if __name__ == "__main__":
+    # Set up logging
+    log = logging.getLogger(__name__)
+    logging.basicConfig(level=logging.DEBUG)
+
+    # debug1()
+    debug2()
