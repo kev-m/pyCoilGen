bcfa2c22eac99989f3ec47b50b8a36db0d83372c
Kevin Meyer <kev-m@users.noreply.github.com>
2023-06-19 12:10:46 +0200

    Same as original MATLAB

--- sub_functions/build_cylinder_mesh.py
+++ sub_functions/build_cylinder_mesh.py
@@ -3,6 +3,14 @@ import numpy as np
 # Logging
 import logging
 
+# TODO: Remove this
+import sys
+from pathlib import Path
+sub_functions_path = Path(__file__).resolve().parent / '..'
+print(sub_functions_path)
+sys.path.append(str(sub_functions_path))
+
+
 # Local imports
 from sub_functions.calc_3d_rotation_matrix_by_vector import calc_3d_rotation_matrix_by_vector
 from sub_functions.data_structures import DataStructure
@@ -35,42 +43,54 @@ def build_cylinder_mesh(
     Returns:
         mesh: DataStructure with 'faces' and 'vertices' arrays of the cylindrical mesh.
     """
-    # Calculate positions along the circular divisions
-    theta = np.linspace(0, 2*np.pi, num_circular_divisions, endpoint=False)
-    x_positions = np.sin(theta) * cylinder_radius
-    y_positions = np.cos(theta) * cylinder_radius
+    # Calculate x, y, z positions of the vertices
+    x_positions = np.sin(np.linspace(0, 2 * np.pi, num_circular_divisions+1)) * cylinder_radius
+    y_positions = np.cos(np.linspace(0, 2 * np.pi, num_circular_divisions+1)) * cylinder_radius
+    x_positions = x_positions[:-1]  # Remove repetition at the end
+    y_positions = y_positions[:-1]  # Remove repetition at the end
+    z_positions = np.linspace(-cylinder_height / 2, cylinder_height / 2,
+                              num_longitudinal_divisions+1)
 
-    # Calculate positions along the longitudinal divisions
-    z_positions = np.linspace(-cylinder_height/2, cylinder_height/2, num_longitudinal_divisions+1)
+    log.debug(" Shape x_positions: %s", np.shape(x_positions))
+    log.debug(" Shape y_positions: %s", np.shape(y_positions))
+    log.debug(" Shape z_positions: %s", np.shape(z_positions))
 
-    # Create vertices
-    vertices_temp = []
-    for z in z_positions:
-        for x, y in zip(x_positions, y_positions):
-            vertices_temp.append([x, y, z])
-    vertices_temp = np.array(vertices_temp)
+    # Create the mesh vertices
+    vertices_x = np.tile(x_positions, num_longitudinal_divisions+1)
+    vertices_y = np.tile(y_positions, num_longitudinal_divisions+1)
+    vertices_z = np.repeat(z_positions, len(x_positions))
+    log.debug(" Shape vertices_x: %s", np.shape(vertices_x))
+    log.debug(" Shape vertices_y: %s", np.shape(vertices_y))
+    log.debug(" Shape vertices_z: %s", np.shape(vertices_z))
+    vertices = np.vstack((vertices_x, vertices_y, vertices_z))
 
-    # Create faces
-    faces = []
-    for i in range(num_longitudinal_divisions):
-        for j in range(num_circular_divisions):
-            a = i * num_circular_divisions + j
-            b = i * num_circular_divisions + (j + 1) % num_circular_divisions
-            c = (i + 1) * num_circular_divisions + j
-            d = (i + 1) * num_circular_divisions + (j + 1) % num_circular_divisions
-            faces.append([a, c, b])
-            faces.append([b, c, d])
-    faces = np.array(faces)
+    # Set the vertices in the center
+    log.debug(" Shape vertices: %s", vertices.shape)
+    vertices = vertices - np.mean(vertices, axis=1, keepdims=True)
+
+    # Create the faces for the cylinder mesh
+    tri_1_vert_inds_1 = np.arange(num_circular_divisions * num_longitudinal_divisions)
+    tri_1_vert_inds_2 = tri_1_vert_inds_1 + 1
+    tri_1_vert_inds_2[tri_1_vert_inds_2 % num_circular_divisions == 0] -= num_circular_divisions
+    tri_1_vert_inds_3 = tri_1_vert_inds_2 + num_circular_divisions
+
+    tri_2_vert_inds_1 = tri_1_vert_inds_1
+    tri_2_vert_inds_2 = tri_1_vert_inds_3
+    tri_2_vert_inds_3 = np.arange(num_circular_divisions * num_longitudinal_divisions) + num_circular_divisions
+
+    faces_1 = np.column_stack((tri_1_vert_inds_2, tri_1_vert_inds_1, tri_1_vert_inds_3))
+    faces_2 = np.column_stack((tri_2_vert_inds_2, tri_2_vert_inds_1, tri_2_vert_inds_3))
+    faces = np.vstack((faces_1, faces_2)) # Subtract 1 due to Matlab index offset starting at 1
 
     # Rotate the cylinder in the desired orientation
     rot_vec = np.array([rotation_vector_x, rotation_vector_y, rotation_vector_z])
     rot_mat = calc_3d_rotation_matrix_by_vector(rot_vec, rotation_angle)
+    vertices=np.dot(vertices.T, rot_mat)
 
     # Calculate representative normal
     normal = np.array([0.0, 0.0, 1.0])
     normal_rep = np.dot(normal, rot_mat)
 
-    vertices=np.dot(vertices_temp, rot_mat)
     cylinder_mesh = DataStructure(vertices=vertices, faces=faces, normal=normal_rep)
 
     return cylinder_mesh
@@ -78,6 +98,44 @@ def build_cylinder_mesh(
 
 
 if __name__ == "__main__":
+    # Set up logging
+    log = logging.getLogger(__name__)
+    logging.basicConfig(level=logging.DEBUG)
+
+    params = [0.4,  0.1125, 50, 50,  0.,  1.,  0., 0.]
+    mesh = build_cylinder_mesh(*params)
+
+    log.debug(" faces: %s, %s, min: %s, max: %s", mesh.faces, mesh.faces.shape, np.min(mesh.faces), np.max(mesh.faces))
+    log.debug(" Should be:\n [2500 2499 2549]], (5000, 3)")
+    
+    log.debug(" vertices: %s, %s", mesh.vertices, mesh.vertices.shape)
+    log.debug(" Should be:\n [-1.40999888e-02  1.11612904e-01  1.96078431e-01]], (2550, 3)")
+
+    from sub_functions.data_structures import Mesh
+    tri_mesh = Mesh(vertices=mesh.vertices, faces=mesh.faces)
+
+    t_faces = tri_mesh.get_faces()
+    log.debug(" t_faces: %s, %s, min: %s, max: %s", t_faces, t_faces.shape, np.min(t_faces), np.max(t_faces))
+    #tri_mesh.display()
+
+"""
+DEBUG:__main__: m_faces: [[   1    0   51]
+ [   2    1   52]
+ [   3    2   53]
+ ...
+ [2548 2497 2547]
+ [2549 2498 2548]
+ [2500 2499 2549]], (5000, 3)
+DEBUG:__main__: m_vertices: [[ 4.14973067e-20  1.12500000e-01 -1.96078431e-01]
+ [ 1.40999888e-02  1.11612904e-01 -1.96078431e-01]
+ [ 2.79776123e-02  1.08965606e-01 -1.96078431e-01]
+ ...
+ [-4.14140122e-02  1.04599855e-01  1.96078431e-01]
+ [-2.79776123e-02  1.08965606e-01  1.96078431e-01]
+ [-1.40999888e-02  1.11612904e-01  1.96078431e-01]], (2550, 3)
+"""    
+
+"""    
     cylinder_height = 2.0
     cylinder_radius = 1.0
     num_circular_divisions = 10
@@ -91,7 +149,7 @@ if __name__ == "__main__":
                                rotation_vector_z, rotation_angle)
     print("vertices = ", mesh.vertices, mesh.vertices.shape)
     print("faces = ", mesh.faces, np.min(mesh.faces), np.max(mesh.faces))
-
+"""
 """
  [[ 1.00000000e+00  0.00000000e+00  0.00000000e+00]
  [ 8.09016994e-01  4.15626938e-01  4.15626938e-01]
