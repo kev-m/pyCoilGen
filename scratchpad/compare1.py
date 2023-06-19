253208ba4afd043b7be28411066d7ab9bfd1b904
Kevin Meyer <kev-m@users.noreply.github.com>
2023-06-19 08:31:35 +0200

    Working with transpose support

--- scratchpad/compare1.py
+++ scratchpad/compare1.py
@@ -11,45 +11,16 @@ def load_matlab(filename):
     return mat
 
 
-def explore(mat_contents, what):
-    log.debug(f" -- {what} Type: %s", type(mat_contents[what]))
-    # log.debug( f" -- {what} Content: %s", mat_contents[what])
-
-
-def print_nested_structure(data, indent=''):
+def print_structure(mat_input, indent_char = ' ', indent = None):
     """
-    Prints the structure of all nested elements within a numpy.ndarray.
+    Prints the structure of all nested elements within a numpy ndarray.
 
     Args:
-        data (numpy.ndarray): The input array.
+        data (numpy.ndarray): The input.
         indent (str): Optional. The current indentation level for printing.
     """
-    if isinstance(data, np.ndarray):
-        print(f'{indent}Array(shape={data.shape}, dtype={data.dtype})')
-        for i, item in enumerate(data):
-            if isinstance(item, (np.number)):
-                break
-            print(f'{indent}Index1 {i}:')
-            print_nested_structure(item, indent + '  ')
-    elif isinstance(data, (list, tuple)):
-        print(f'{indent}List or Tuple (length={len(data)})')
-        for i, item in enumerate(data):
-            print(f'{indent}Index2 {i}:')
-            print_nested_structure(item, indent + '  ')
-    elif isinstance(data, dict):
-        print(f'{indent}Dictionary (keys={list(data.keys())})')
-        for key, value in data.items():
-            print(f'{indent}Key: {key}')
-            print_nested_structure(value, indent + '  ')
-    elif isinstance(data, (np.uint16)):
-        pass
-    else:
-        print(f'{indent}Value: {data.dtype}')
-        # print(f'{indent}Value: not shown')
-        print_nested_structure(data[0], indent + '  ')
-
-
-def find_field(mat_input, seek_field_name, indent = ' '):
+    if indent is None:
+        indent = indent_char
     for item in mat_input:
         try:
             dtype = item.dtype
@@ -57,18 +28,18 @@ def find_field(mat_input, seek_field_name, indent = ' '):
             if fields is not None:
                 index = 0
                 for field_name, field_info in fields.items():
-                    print(f'{indent}Field: {field_name}')
+                    print(f'{indent}: {field_name}')
                     try:
                         next_item = item[index]
                         if isinstance(next_item, object):
-                            find_field(next_item, seek_field_name, indent+' ')
+                            print_structure(next_item, indent+indent_char, indent_char)
                             index += 1
                     except IndexError:
                         return
         except AttributeError:
             return
             
-def get_element_by_name_internal(data, parts):
+def get_element_by_name_internal(data, parts, transpose=True):
     key_len = len(parts)
     if key_len == 0:
         log.debug(" No more children")
@@ -94,22 +65,38 @@ def get_element_by_name_internal(data, parts):
                 if key_len == 1:
                     log.debug("Returning data[%s][%d]", field_name, key_index)
                     return data[field_name][key_index][0]
-                    # index 13 is out of bounds for axis 0 with size 1
-                    #return data[key_index][field_index]
                 else:
                     return get_element_by_name_internal(data[field_index][key_index][0], parts[1:])
             field_index += 1
         raise AttributeError(f"Key {key} not found!")
     else:
-        log.error("Not handled")
-        raise Exception("Not implemented yet")
-    log.debug(" Dunno")
-    return  get_element_by_name_internal(current_element, parts[1:])
+        log.debug(" -- %s", np.shape(data[key_index]))
+        if transpose and isinstance(data, (np.ndarray)):
+            return data.T[key_index]
+        return data[key_index]
 
-def get_element_by_name(data, name):
+def get_element_by_name(np_data_array, name, transpose=True):
+    """
+    Finds and returns the field with the given name from the given np_data_array.
+
+    The field name is specified in dot notation: 'first.second.third'.
+    Array indexing is supported: 'first.second[1].third'.
+
+    Args:
+        np_data_array (numpy.ndarray): The input.
+        name (str): The desired field name in dot notation.
+        transpose (bool): Transpose the result, if it is an array.
+
+    Raises:
+        AttributeError: If the key is not found.
+        IndexError: If the array index is invalid.
+    """
     log.debug(" Searching for %s", name)
     elements = name.split('.')
-    return get_element_by_name_internal(data=data, parts=elements)
+    result = get_element_by_name_internal(data=np_data_array, parts=elements, transpose=transpose)
+    if transpose and isinstance(result, (np.ndarray)):
+        return result.T
+    return result
 
 if __name__ == "__main__":
     # Set up logging
@@ -120,38 +107,26 @@ if __name__ == "__main__":
     # 1. Load Matlab
     mat_contents = load_matlab('debug/result_x_gradient')
     log.debug("mat_contents: %s", mat_contents.keys())
-    # explore(mat_contents, '__globals__')
-    explore(mat_contents, 'x_channel')
-
     x_channel = mat_contents['x_channel']
-    log.debug(f" -- x_channel[0] Type: %s", type(x_channel))
-    # log.debug( f" -- x_channel[0] : %s", x_channel[0][0])
-    log.debug(f" -- x_channel.dtype : %s", x_channel[0].dtype)
 
-    # print_nested_structure(x_channel)
-    find_field(x_channel, 'input')
-
-    # xxx = str(x_channel[0])
-    # print(dir(xxx))
-    yyy = x_channel[0][0][0][0][0][0][0][0][0][0][0]
-    # print(dir(yyy))
-    xxx = str(yyy.dtype)
-    # print (xxx[0:100])
-    print("xxx:", xxx)
-    print("yyy['boundary'].dtype:", yyy['boundary'].dtype)
-    print(x_channel['out'][0].dtype)
+    print_structure(x_channel, '-')
 
     # Dot-notation name
     name = 'out'
     name = 'out.coil_parts[0]'
     name = 'out.coil_parts[0].coil_mesh'
     name = 'out.coil_parts[0].coil_mesh.boundary'
-    #name = 'out.coil_parts[0].coil_mesh.faces'
+    #name = 'out.coil_parts[0].coil_mesh.faces[1]'
+    #name = 'out.coil_parts[0].coil_mesh.faces.faces[0]'
+    #name = 'out.coil_parts[0].coil_mesh.unique_vert_inds'
+    name = 'out.coil_parts[0].coil_mesh.uv'
+    #name = 'out.coil_parts[0].one_ring_list'
 
     # Get the corresponding numpy element
-    input_data = get_element_by_name(x_channel, name)
+    input_data = get_element_by_name(x_channel, name, transpose=True)
     
     log.debug(" -- input_data.dtype : %s", input_data.dtype)
+    log.debug("  -- shape: %s", input_data.shape)
     log.debug("  -- value: %s", input_data)
 
     # 2. Extract input parameters structure
