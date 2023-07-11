5acc66614889b0bce1b461ded97771178d728418
Kevin Meyer <kev-m@users.noreply.github.com>
2023-07-11 14:30:21 +0200

    Reformatted

--- sub_functions/define_target_field.py
+++ sub_functions/define_target_field.py
@@ -28,8 +28,8 @@ def define_target_field(coil_parts, target_mesh, secondary_target_mesh, input):
         input (object): Input parameters.
 
     Returns:
-        target_field_out (object): Target field information.
-        is_supressed_point (ndarray): Array indicating whether a point is suppressed or not.
+        target_field_out (ndarray): Target field information (3 x m).
+        is_supressed_point (ndarray): Array indicating whether a point is suppressed or not (m).
     """
     target_field_out = TargetField()
 
@@ -99,8 +99,8 @@ def define_target_field(coil_parts, target_mesh, secondary_target_mesh, input):
             target_z_coords = np.linspace(-2, 2, 4*(num_points_per_dim-1)+1) * input.target_region_radius
             target_grid_x, target_grid_y, target_grid_z = np.meshgrid(target_x_coords, target_y_coords, target_z_coords)
 
-            # target_points = np.vstack((target_grid_x.ravel(), target_grid_y.ravel(), target_grid_z.ravel()))
             # For some unknown reason I need to swap y and z coords to match MATLAB
+            # target_points = np.vstack((target_grid_x.ravel(), target_grid_y.ravel(), target_grid_z.ravel()))
             target_points = np.vstack((target_grid_x.ravel(), target_grid_z.ravel(), target_grid_y.ravel()))
 
             # Select points inside a sphere
@@ -113,7 +113,7 @@ def define_target_field(coil_parts, target_mesh, secondary_target_mesh, input):
             all_verts = np.vstack([part.coil_mesh.get_vertices() for part in coil_parts])
 
             if input.set_roi_into_mesh_center:
-                mean_pos = np.mean(all_verts, axis=0, keepdims=True) # 
+                mean_pos = np.mean(all_verts, axis=0, keepdims=True)
                 target_points3 = target_points2 - mean_pos.T
             else:
                 target_points3 = target_points2
@@ -125,48 +125,48 @@ def define_target_field(coil_parts, target_mesh, secondary_target_mesh, input):
 
         # Define the target field shape
         def field_func(x, y, z): return eval(input.field_shape_function)
-        target_field=np.zeros_like(target_points)
-        target_field[2, :]=field_func(target_points[0, :], target_points[1, :], target_points[2, :])
+        target_field = np.zeros_like(target_points)
+        target_field[2, :] = field_func(target_points[0, :], target_points[1, :], target_points[2, :])
 
         # Add points where the magnetic field should be suppressed (=>0)
         if secondary_target_mesh is not None:
-            num_suppressed_points=secondary_target_mesh.get_vertices().shape[1]
-            target_points=np.hstack((target_points, secondary_target_mesh.get_vertices()))
-            target_field=np.hstack((target_field, np.zeros((target_field.shape[0], num_suppressed_points))))
-            is_supressed_point=np.zeros(target_points.shape[1], dtype=bool)
-            is_supressed_point[-num_suppressed_points:]=True
+            num_suppressed_points = secondary_target_mesh.get_vertices().shape[1]
+            target_points = np.hstack((target_points, secondary_target_mesh.get_vertices()))
+            target_field = np.hstack((target_field, np.zeros((target_field.shape[0], num_suppressed_points))))
+            is_supressed_point = np.zeros(target_points.shape[1], dtype=bool)
+            is_supressed_point[-num_suppressed_points:] = True
         else:
-            is_supressed_point=np.zeros(target_points.shape[1], dtype=bool)
+            is_supressed_point = np.zeros(target_points.shape[1], dtype=bool)
 
         # Scale the fields to a targeted strength
-        max_field_point_ind=np.argmax(target_field[2, :])
-        min_field_point_ind=np.argmin(target_field[2, :])
-        max_target_distance=np.linalg.norm(
+        max_field_point_ind = np.argmax(target_field[2, :])
+        min_field_point_ind = np.argmin(target_field[2, :])
+        max_target_distance = np.linalg.norm(
             target_points[:, max_field_point_ind] - target_points[:, min_field_point_ind])
-        max_field_difference=np.max(target_field[2, :]) - np.min(target_field[2, :])
+        max_field_difference = np.max(target_field[2, :]) - np.min(target_field[2, :])
 
         if abs(max_field_difference) > 10**(-10):
-            target_field=target_field / (max_field_difference / max_target_distance) * input.target_gradient_strength
+            target_field = target_field / (max_field_difference / max_target_distance) * input.target_gradient_strength
         else:
-            target_field=target_field * input.target_gradient_strength
+            target_field = target_field * input.target_gradient_strength
 
         # Define weightings from 0 to 1 that weights the significance of target points
-        target_field_weighting=np.ones(target_field.shape[1])
-        target_field_weighting[is_supressed_point]=input.secondary_target_weight
-        target_field_group_inds=np.ones(target_field.shape[1])
-        target_field_group_inds[is_supressed_point]=2
+        target_field_weighting = np.ones(target_field.shape[1])
+        target_field_weighting[is_supressed_point] = input.secondary_target_weight
+        target_field_group_inds = np.ones(target_field.shape[1])
+        target_field_group_inds[is_supressed_point] = 2
 
         # Calculate the gradients from the symbolic definition of the target field
-        target_dbzbx, target_dbzby, target_dbzbz=symbolic_calculation_of_gradient(input, target_field)
+        target_dbzbx, target_dbzby, target_dbzbz = symbolic_calculation_of_gradient(input, target_field)
 
         if get_level() > DEBUG_BASIC:
             log.debug(" Final target_field:\n%s", target_field[:, :5])
             log.debug(" Final target_points:\n%s", target_points[:, :5])
-        target_field_out.b=target_field
-        target_field_out.coords=target_points
-        target_field_out.weights=target_field_weighting
-        target_field_out.target_field_group_inds=target_field_group_inds
-        target_field_out.target_gradient_dbdxyz=np.array([target_dbzbx, target_dbzby, target_dbzbz])
+        target_field_out.b = target_field
+        target_field_out.coords = target_points
+        target_field_out.weights = target_field_weighting
+        target_field_out.target_field_group_inds = target_field_group_inds
+        target_field_out.target_gradient_dbdxyz = np.array([target_dbzbx, target_dbzby, target_dbzbz])
 
     return target_field_out, is_supressed_point
 
@@ -186,28 +186,28 @@ def symbolic_calculation_of_gradient(input, target_field):
     """
     try:
 
-        x, y, z=symbols('x y z')
-        dbzdx_expr=diff(input.field_shape_function, x)
-        dbzdy_expr=diff(input.field_shape_function, y)
-        dbzdz_expr=diff(input.field_shape_function, z)
+        x, y, z = symbols('x y z')
+        dbzdx_expr = diff(input.field_shape_function, x)
+        dbzdy_expr = diff(input.field_shape_function, y)
+        dbzdz_expr = diff(input.field_shape_function, z)
 
         # Convert expressions to string representations
-        dbzdx_str=str(dbzdx_expr)
-        dbzdy_str=str(dbzdy_expr)
-        dbzdz_str=str(dbzdz_expr)
+        dbzdx_str = str(dbzdx_expr)
+        dbzdy_str = str(dbzdy_expr)
+        dbzdz_str = str(dbzdz_expr)
 
         # Modify string representations for array-wise operations
-        dbzdx_str=dbzdx_str.replace("/", "./")
-        dbzdx_str=dbzdx_str.replace("^", ".^")
-        dbzdx_str=dbzdx_str.replace("*", ".*")
+        dbzdx_str = dbzdx_str.replace("/", "./")
+        dbzdx_str = dbzdx_str.replace("^", ".^")
+        dbzdx_str = dbzdx_str.replace("*", ".*")
 
-        dbzdy_str=dbzdy_str.replace("/", "./")
-        dbzdy_str=dbzdy_str.replace("^", ".^")
-        dbzdy_str=dbzdy_str.replace("*", ".*")
+        dbzdy_str = dbzdy_str.replace("/", "./")
+        dbzdy_str = dbzdy_str.replace("^", ".^")
+        dbzdy_str = dbzdy_str.replace("*", ".*")
 
-        dbzdz_str=dbzdz_str.replace("/", "./")
-        dbzdz_str=dbzdz_str.replace("^", ".^")
-        dbzdz_str=dbzdz_str.replace("*", ".*")
+        dbzdz_str = dbzdz_str.replace("/", "./")
+        dbzdz_str = dbzdz_str.replace("^", ".^")
+        dbzdz_str = dbzdz_str.replace("*", ".*")
 
         # DEBUG
         if input.debug >= DEBUG_BASIC:
@@ -216,29 +216,29 @@ def symbolic_calculation_of_gradient(input, target_field):
             log.debug(' - dbzdz_fun: %s', dbzdz_str)
 
         # Define lambdify functions for array-wise operations
-        dbzdx_fun=lambdify((x, y, z), dbzdx_str)
-        dbzdy_fun=lambdify((x, y, z), dbzdy_str)
-        dbzdz_fun=lambdify((x, y, z), dbzdz_str)
+        dbzdx_fun = lambdify((x, y, z), dbzdx_str)
+        dbzdy_fun = lambdify((x, y, z), dbzdy_str)
+        dbzdz_fun = lambdify((x, y, z), dbzdz_str)
 
         # Evaluate the lambdify functions
-        target_dbzbx=dbzdx_fun(target_field[0, :], target_field[1, :], target_field[2, :])
-        target_dbzby=dbzdy_fun(target_field[0, :], target_field[1, :], target_field[2, :])
-        target_dbzbz=dbzdz_fun(target_field[0, :], target_field[1, :], target_field[2, :])
+        target_dbzbx = dbzdx_fun(target_field[0, :], target_field[1, :], target_field[2, :])
+        target_dbzby = dbzdy_fun(target_field[0, :], target_field[1, :], target_field[2, :])
+        target_dbzbz = dbzdz_fun(target_field[0, :], target_field[1, :], target_field[2, :])
 
         if is_multivalued(target_dbzbx) == False:
-            target_dbzbx=np.repeat(target_dbzbx, target_field.shape[1])
+            target_dbzbx = np.repeat(target_dbzbx, target_field.shape[1])
 
         if is_multivalued(target_dbzby) == False:
-            target_dbzby=np.repeat(target_dbzby, target_field.shape[1])
+            target_dbzby = np.repeat(target_dbzby, target_field.shape[1])
 
         if is_multivalued(target_dbzbz) == False:
-            target_dbzbz=np.repeat(target_dbzbz, target_field.shape[1])
+            target_dbzbz = np.repeat(target_dbzbz, target_field.shape[1])
 
     except Exception as e:
         log.error(' Exception: %s', e)
-        target_dbzbx=np.zeros_like(target_field[2, :])
-        target_dbzby=np.zeros_like(target_field[2, :])
-        target_dbzbz=np.zeros_like(target_field[2, :])
+        target_dbzbx = np.zeros_like(target_field[2, :])
+        target_dbzby = np.zeros_like(target_field[2, :])
+        target_dbzbz = np.zeros_like(target_field[2, :])
         log.error('Gradient Calculation from Symbolic Target failed')
 
     return target_dbzbx, target_dbzby, target_dbzbz
