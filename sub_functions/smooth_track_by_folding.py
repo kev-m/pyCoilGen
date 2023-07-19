e1e11f029c1fc19ba5cc03d9664019716f75b836
Kevin Meyer <kev-m@users.noreply.github.com>
2023-07-19 16:05:00 +0200

    Reimplemented and only acts if smoothing greater than 1,

--- sub_functions/smooth_track_by_folding.py
+++ sub_functions/smooth_track_by_folding.py
@@ -1,28 +1,39 @@
 import numpy as np
 
-
 def smooth_track_by_folding(track_in, smoothing_length):
     """
     Smooth a track by folding its data.
 
     Args:
-        track_in (ndarray): The input track data with shape (2, m).
+        track_in (ndarray): The input track data with shape (2, M).
         smoothing_length (int): The length of smoothing.
 
     Returns:
-        ndarray: The smoothed track with shape (2, m).
+        ndarray: The smoothed track with shape (2, M).
     """
-    track_out = track_in.copy()
 
     if smoothing_length > 1:
-        extended_track = np.concatenate((np.tile(track_in[:, 0][:, np.newaxis], smoothing_length),
-                                        track_in[:, 1:-1], np.tile(track_in[:, -1][:, np.newaxis], smoothing_length)), axis=1)
+        track_out = track_in.copy()
+        num_points = track_in.shape[1]
+
+        # Extend the track by repeating the first and last points for smoothing
+        extended_track = np.hstack([np.tile(track_in[:, 0].reshape(-1, 1), (1, smoothing_length)), 
+                                    track_in, 
+                                    np.tile(track_in[:, -1].reshape(-1, 1), (1, smoothing_length))])
 
         for shift_ind in range(-(smoothing_length - 1), smoothing_length):
+            # Shift the extended track to create overlapping segments
             add_track = np.roll(extended_track, shift_ind, axis=1)
-            add_track = add_track[:, smoothing_length:(-smoothing_length + 1)]
+
+            # Remove the overlapping parts to avoid double counting
+            add_track = add_track[:, smoothing_length:(num_points + smoothing_length)]
+
+            # Add the overlapping segments to the original track_out
             track_out += add_track
 
-        track_out /= (2 * smoothing_length - 1)
+        # Calculate the average of the overlapping segments
+        track_out /= (2 * smoothing_length)
+        return track_out
+    else:
+        return track_in
 
-    return track_out
