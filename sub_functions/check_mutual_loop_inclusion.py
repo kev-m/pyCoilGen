67895e893d8e81f2724eeea95a437e045fbb1d71
Kevin Meyer <kev-m@users.noreply.github.com>
2023-07-27 10:18:59 +0200

    Regenerated by accident.

--- sub_functions/check_mutual_loop_inclusion.py
+++ sub_functions/check_mutual_loop_inclusion.py
@@ -1,18 +1,28 @@
 import numpy as np
 from typing import List
 
+
 def check_mutual_loop_inclusion(test_poly: np.ndarray, target_poly: np.ndarray) -> bool:
     """
     Check if the test polygon lies fully enclosed within the second polygon.
     This check is done with the winding number algorithm test for each vertex towards the second polygon.
 
-    Args:
-        test_poly (np.ndarray): The polygon to test for inclusion.
-        target_poly (np.ndarray): The target polygon.
+    Parameters:
+        test_poly (np.ndarray): Array representing the test polygon's 2D coordinates (shape: (2, num_vertices)).
+        target_poly (np.ndarray): Array representing the target polygon's 2D coordinates (shape: (2, num_vertices)).
 
     Returns:
         bool: True if the test polygon is fully enclosed within the target polygon, False otherwise.
+
+    Example:
+        # Define the test polygon and target polygon coordinates as 2D arrays
+        test_poly = np.array([[x1, x2, x3, ...], [y1, y2, y3, ...]])
+        target_poly = np.array([[xt1, xt2, xt3, ...], [yt1, yt2, yt3, ...]])
+
+        # Check if the test polygon is fully enclosed within the target polygon
+        inside_flag = check_mutual_loop_inclusion(test_poly, target_poly)
     """
+
     winding_numbers = np.zeros(test_poly.shape[1])
 
     for point_ind in range(test_poly.shape[1]):
@@ -20,14 +30,22 @@ def check_mutual_loop_inclusion(test_poly: np.ndarray, target_poly: np.ndarray) 
         B = target_poly[:, 1:]
         C = target_poly[:, :-1]
 
-        # ValueError: operands could not be broadcast together with shapes (2,7) (1,14) 
         vec1 = C - A
         vec2 = B - A
 
-        angle = np.arctan2(vec1[0, :] * vec2[1, :] - vec1[1, :] * vec2[0, :], vec1[0, :] * vec2[0, :] + vec1[1, :] * vec2[1, :])
+        angle = np.arctan2(vec1[0, :] * vec2[1, :] - vec1[1, :] * vec2[0, :],
+                           vec1[0, :] * vec2[0, :] + vec1[1, :] * vec2[1, :])
 
         winding_numbers[point_ind] = round(abs(np.sum(angle) / (2 * np.pi)))
 
     inside_flag = np.all(winding_numbers == 1)
 
     return inside_flag
+
+"""
+In this Python implementation, the function check_mutual_loop_inclusion takes two 2D arrays, test_poly and target_poly,
+which represent the coordinates of the test polygon and the target polygon, respectively. The function calculates the
+winding numbers for each vertex of the test polygon with respect to the target polygon using the winding number
+algorithm. If all winding numbers are equal to 1, it indicates that the test polygon is fully enclosed within the 
+target polygon, and the function returns True; otherwise, it returns False.
+"""
