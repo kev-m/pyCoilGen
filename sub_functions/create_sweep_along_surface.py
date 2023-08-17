150d13f0a2ba6da89533e89fc51df50658f303ae
Kevin Meyer <kev-m@users.noreply.github.com>
2023-08-17 16:16:26 +0200

    Debugging possible issue with point_inside_triangle

--- sub_functions/create_sweep_along_surface.py
+++ sub_functions/create_sweep_along_surface.py
@@ -11,8 +11,9 @@ import logging
 
 log = logging.getLogger(__name__)
 
-# TODO: DEBUG Remove this
-from helpers.visualisation import compare
+# TODO: DEBUG Remove these
+from helpers.visualisation import compare, visualize_faces
+from sub_functions.data_structures import Shape2D, Shape3D
 
 def create_sweep_along_surface(coil_parts: List[CoilPart], input_args, m_c_part = None) -> List[CoilPart]:
     """
@@ -44,7 +45,9 @@ def create_sweep_along_surface(coil_parts: List[CoilPart], input_args, m_c_part 
         for part_ind in range(len(coil_parts)):
             coil_part = coil_parts[part_ind]
             coil_mesh = coil_part.coil_mesh
-            wire_path = coil_part.wire_path
+            # wire_path = coil_part.wire_path
+            log.warning(" Using MATLAB wirepath")
+            wire_path = Shape3D(v=m_c_part.wire_path.v, uv=m_c_part.wire_path.uv) 
 
             # DEBUG
             if m_c_part is not None:
@@ -159,6 +162,7 @@ def create_sweep_along_surface(coil_parts: List[CoilPart], input_args, m_c_part 
             # DEBUG
             if m_c_part is not None:
                 assert np.isclose(cross_section_area, m_debug_out.cross_section_area)
+                assert np.isclose(wire_path.v_length, m_debug_out.wire_path1.v_length)
 
 
             # Calculate a radius of the conductor cross section which is later
@@ -170,6 +174,8 @@ def create_sweep_along_surface(coil_parts: List[CoilPart], input_args, m_c_part 
             # DEBUG
             if m_c_part is not None:
                 assert compare(cross_section_center, m_debug_out.cross_section_center[:2])
+                assert compare(wire_path.v.T, m_debug_out.wire_path1.v.T) # 
+                assert cross_section_radius == m_debug_out.cross_section_radius
 
 
             # Remove repeating entries
@@ -178,22 +184,94 @@ def create_sweep_along_surface(coil_parts: List[CoilPart], input_args, m_c_part 
             wire_path.v = np.delete(wire_path.v, repeat_point_indices, axis=1)
             wire_path.uv = np.delete(wire_path.uv, repeat_point_indices, axis=1)
 
+            # DEBUG
+            if m_c_part is not None:
+                assert compare(wire_path.v.T, m_debug_out.wire_path2.v.T) # 
+
             # Open the track if it's not already opened
-            point_inds_to_delete = np.where(np.linalg.norm(
-                wire_path.v[:, -1][:, np.newaxis] - wire_path.v) < cross_section_radius / 2)[0]
+            diff2_arr = wire_path.v[:, -1][:, np.newaxis] - wire_path.v
+            diff2_norm = np.linalg.norm(diff2_arr, axis=0)
+            arr2 =(diff2_norm < cross_section_radius / 2).astype(int)
+            point_inds_to_delete = np.where(diff2_norm < cross_section_radius / 2)[0]
+
+            # DEBUG
+            if m_c_part is not None:
+                assert compare(diff2_arr, m_debug_out.diff2_arr)    # 
+                assert compare(diff2_norm, m_debug_out.diff2_norm)  # 
+                assert compare(arr2, m_debug_out.point_inds_to_delete1) # M 1539 array of 0 or 1
+
             point_inds_to_delete = point_inds_to_delete[:round(len(point_inds_to_delete) / 2)]
+
+            # DEBUG
+            if m_c_part is not None:
+                assert compare(point_inds_to_delete, m_debug_out.point_inds_to_delete2) # Y
+
             wire_path.v = np.delete(wire_path.v, point_inds_to_delete, axis=1)
             wire_path.uv = np.delete(wire_path.uv, point_inds_to_delete, axis=1)
 
             # Calculate the normal vectors along the wire track
             surface_normal_along_wire_path_v = np.zeros((3, wire_path.v.shape[1])) # MATLAB shape
 
+            # planary_mesh_matlab_format = triangulation(parameterized_mesh.faces', parameterized_mesh.uv');
+            wire_mesh2D = Mesh(vertices=coil_mesh.uv, faces=coil_mesh.get_faces())
+
+            # DEBUG
+            if m_c_part is not None:
+                assert compare(wire_path.v.T, m_debug_out.wire_path3.v.T, double_tolerance=0.001) # Y
+                assert compare(coil_mesh.uv, m_c_part.coil_mesh.uv.T) # ???
+                assert compare(coil_mesh.get_faces(), m_c_part.coil_mesh.faces.T-1) # ???
+
+                m_found_normals = m_debug_out.found_normals - 1
+                m_found_points = m_debug_out.found_points.T
+                a = 0
+                b = 0
+                zap = 3
+
             for point_ind in range(wire_path.v.shape[1]):
-                #node_ind_normals_target = planary_mesh_matlab_format.find_simplex(wire_path.uv[:, point_ind])
+                #log.debug(" point %d", point_ind)
+                # node_ind_normals_target = pointLocation(planary_mesh_matlab_format, point_x, point_y);
                 point = wire_path.uv[:, point_ind]
-                node_ind_normals_target = coil_mesh.get_vertex_index(point)
+                node_ind_normals_target, possible_face_indices, faces_to_try = wire_mesh2D.get_face_index(point)
+
+                # DEBUG
+                if m_c_part is not None:
+                    assert compare(point, m_found_points[point_ind])
+                    if node_ind_normals_target != m_found_normals[point_ind]:
+                        if a < zap:
+                            log.debug(" faces don't match: %s, %s <> %s - %s", point_ind, node_ind_normals_target, m_found_normals[point_ind], possible_face_indices)
+                            p_id = node_ind_normals_target
+                            p_face_indices = coil_mesh.get_faces()[p_id]
+                            p_vertices = coil_mesh.uv[p_face_indices]
+
+                            m_id = m_found_normals[point_ind]
+                            m_face_indices = (m_c_part.coil_mesh.faces.T-1)[m_id]
+                            m_vertices = coil_mesh.uv[m_face_indices]
+
+                            p_faces = [p_vertices]
+                            m_faces = [m_vertices]
+
+                            centres = point.reshape(1,2).T
+                            
+                            visualize_faces(p_faces, 800, f'images/19_get_face_index1_{point_ind}_p.png', centres)
+                            visualize_faces(m_faces, 800, f'images/19_get_face_index1_{point_ind}_m.png', centres)
+
+                            a += 1
+
 
                 if node_ind_normals_target == -1:  # Handle exceptions for strange output of pointLocation
+
+                    if b < zap:
+                        log.debug(" No face found: %d out of %d", point_ind, len(faces_to_try))
+                        centres = point.reshape(1,2).T
+                        p_contour = Shape2D()
+                        x_uv = Shape2D()
+                        face_vertices = []
+                        for face in coil_mesh.uv[faces_to_try]:
+                            face_vertices.append(face)
+                        visualize_faces(face_vertices, 800, f'images/19_get_face_index2_{point_ind}_p.png', centres)
+                        b += 1
+
+
                     if point_ind == 0:
                         surface_normal_along_wire_path_v[:,
                                                          point_ind] = surface_normal_along_wire_path_v[:, point_ind + 1]
@@ -202,6 +280,17 @@ def create_sweep_along_surface(coil_parts: List[CoilPart], input_args, m_c_part 
                                                          point_ind] = surface_normal_along_wire_path_v[:, point_ind - 1]
                 else:
                     surface_normal_along_wire_path_v[:, point_ind] = coil_mesh.fn[node_ind_normals_target]
+
+
+            # DEBUG
+            if m_c_part is not None:
+                assert compare(coil_mesh.fn, m_c_part.coil_mesh.fn)
+                ## FAIL The face search above yields different faces... continuing.
+                # assert compare(surface_normal_along_wire_path_v.T, m_debug_out.surface_normal_alonge_wire_path.v.T, double_tolerance=0.01)
+
+            # DEBUG
+            #log.warning(" Using MATLAB surface normals")
+            #surface_normal_along_wire_path_v = m_debug_out.surface_normal_alonge_wire_path.v
 
             # Continue with Part 2
             """
@@ -225,8 +314,8 @@ def create_sweep_along_surface(coil_parts: List[CoilPart], input_args, m_c_part 
             face_points = np.zeros((wire_path.v.shape[1], wire_path.v.shape[0],
                                    cross_section_points.shape[1]))  # initialize the face points
             all_node_points = np.zeros((wire_path.v.shape[1] * cross_section_points.shape[1], 3))
-            run_ind = 0
 
+            run_ind = 0
             for point_ind in range(wire_path.v.shape[1]):
                 e1 = path_directions[:, point_ind]
                 e2 = surface_normal_along_wire_path_v[:, point_ind]
@@ -235,8 +324,9 @@ def create_sweep_along_surface(coil_parts: List[CoilPart], input_args, m_c_part 
 
                 # Rotate the face points to have the right orientation
                 for aaaa in range(cross_section_points.shape[1]):
-                    face_points[point_ind, :, aaaa] = np.dot(
-                        np.column_stack((e2, e3, e1)), cross_section_points[:, aaaa])
+                    arr1 = np.column_stack((e2, e3, e1))
+                    arr2 = np.hstack((cross_section_points[:, aaaa], 0))
+                    face_points[point_ind, :, aaaa] = np.dot(arr1, arr2)
 
                 # Shift the oriented corner points to the position in the wire path
                 for aaaa in range(cross_section_points.shape[1]):
@@ -263,7 +353,7 @@ def create_sweep_along_surface(coil_parts: List[CoilPart], input_args, m_c_part 
 
             run_ind = 1
 
-            for track_ind in range(face_points.shape[0] - 1):
+            for track_ind in range(face_points.shape[0] - 2):
                 for edge_ind in range(face_points.shape[2]):
                     node_a = track_ind * num_corners + full_edge_inds[edge_ind]
                     node_b = track_ind * num_corners + full_edge_inds[(edge_ind + 1) % num_corners]
@@ -314,8 +404,16 @@ def create_sweep_along_surface(coil_parts: List[CoilPart], input_args, m_c_part 
                 stl_file_path_layout = path.join(output_directory, f"swept_layout_part{part_ind}_{filename}.stl")
                 stl_file_path_surface = path.join(output_directory, f"surface_part{part_ind}_{filename}.stl")
 
+                layout_surface_mesh.cleanup()
                 layout_surface_mesh.export(stl_file_path_layout)
                 coil_mesh.export(stl_file_path_surface)
+
+                # DEBUG
+                layout_surface_mesh.display()
+                #swept_vertices=m_debug_out.swept_vertices
+                #swept_faces=m_debug_out.swept_faces-1
+                #mat_mesh = Mesh(vertices=swept_vertices, faces=swept_faces)
+                #mat_mesh.display()
 
             # Assign outputs
             coil_part.layout_surface_mesh = layout_surface_mesh
