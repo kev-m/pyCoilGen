37148fb230457ca83d9ecb5a62b193ac1ccc82bf
Kevin Meyer <kev-m@users.noreply.github.com>
2023-08-26 20:13:40 +0200

    Adding blkdiag, equivalent to MATLAB function

--- helpers/common.py
+++ helpers/common.py
@@ -1,4 +1,4 @@
-from numpy import dot, sum, ndarray
+from numpy import dot, sum, ndarray, zeros
 
 
 def nearest_approaches(point: ndarray, starts: ndarray, ends: ndarray):
@@ -19,3 +19,27 @@ def nearest_approaches(point: ndarray, starts: ndarray, ends: ndarray):
     vec_targets2 = point - starts
     t1 = sum(vec_targets2 * diffs, axis=0) / sum(diffs * diffs, axis=0)
     return t1, diffs
+
+def blkdiag(arr1: ndarray, arr2:ndarray)->ndarray:
+    """
+    Compute the block diagonal matrix created by aligning the input matrices along the diagonal.
+
+    Args:
+        arr1 (ndarray): The first input matrix.
+        arr2 (ndarray): The second input matrix.
+
+    Returns:
+        ndarray: The block diagonal matrix created by aligning arr1 and arr2 along the diagonal.
+    """
+    # Get the dimensions of the arrays
+    rows1, cols1 = arr1.shape
+    rows2, cols2 = arr2.shape
+
+    # Create a larger zero-filled array with the final desired size
+    result = zeros((rows1 + rows2, cols1 + cols2))
+
+    # Place the smaller array within the larger one
+    result[:rows1, :cols1] = arr1
+    result[rows1:, cols1:] = arr2
+
+    return result
