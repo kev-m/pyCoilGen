5bb1de7b2e607136953ff472125cdaa8caf2b96d
Kevin Meyer <kev-m@users.noreply.github.com>
2023-08-29 19:32:44 +0200

    Setting output directory

--- examples/preoptimzed_breast_coil.py
+++ examples/preoptimzed_breast_coil.py
@@ -60,6 +60,7 @@ if __name__ == '__main__':
         'project_name': 'Preoptimzed_Breast_Coil',
         'fasthenry_bin': '../FastHenry2/bin/fasthenry',
         'persistence_dir': 'debug',
+        'output_directory': 'images',
         'debug': DEBUG_BASIC,
     }
 
