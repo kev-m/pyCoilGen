283c534284bf7518ddcf11b85bdbdb25034132af
Kevin Meyer <kev-m@users.noreply.github.com>
2023-09-15 12:26:46 +0200

    Reduced to only changed parameters

--- examples/ygradient_coil.py
+++ examples/ygradient_coil.py
@@ -29,40 +29,31 @@ if __name__ == '__main__':
     # logging.basicConfig(level=logging.INFO)
 
     arg_dict = {
-        'field_shape_function': 'y',  # % definition of the target field
+        'field_shape_function': 'y',            # % definition of the target field ['x']
         'coil_mesh_file': 'cylinder_radius500mm_length1500mm.stl',
-        'target_mesh_file': 'none',
-        'secondary_target_mesh_file': 'none',
-        'secondary_target_weight': 0.5,
-        'target_region_radius': 0.15,  # ...  % in meter
-        # 'target_region_resolution': 5,  # MATLAB 10 is the default
-        'use_only_target_mesh_verts': False,
-        'sf_source_file': 'none',
-        # % the number of potential steps that determines the later number of windings (Stream function discretization)
-        'levels': 20,
-        'pot_offset_factor': 0.25,  # % a potential offset value for the minimal and maximal contour potential ; must be between 0 and 1
-        'surface_is_cylinder_flag': True,
-        'interconnection_cut_width': 0.1,  # % the width for the interconnections are interconnected; in meter
-        'normal_shift_length': 0.025,  # % the length for which overlapping return paths will be shifted along the surface normals; in meter
-        'iteration_num_mesh_refinement': 1,  # % the number of refinements for the mesh;
-        'set_roi_into_mesh_center': True,
-        'force_cut_selection': ['high'],  # ...
-        'level_set_method': 'primary',  # ... %Specify one of the three ways the level sets are calculated: "primary","combined", or "independent"
-        'skip_postprocessing': False,
-        'skip_inductance_calculation': False,
-        'make_cylindrical_pcb': True,
-        'conductor_cross_section_width': 0.015,
+        'secondary_target_weight': 0.5,         # [1.0]
+        'target_region_resolution': 10,        # MATLAB 10 is the default
+        'levels': 20,                           # The number of potential steps, determines the number of windings [10]
+        # a potential offset value for the minimal and maximal contour potential [0.5]
+        'pot_offset_factor': 0.25,
+        'interconnection_cut_width': 0.1,       # Width cut used when cutting and joining wire paths; in metres [0.01]
+        # Displacement that overlapping return paths will be shifted along the surface normals; in meter [0.001]
+        'normal_shift_length': 0.025,
+        'iteration_num_mesh_refinement': 1,     # % the number of refinements for the mesh; [0]
+        'set_roi_into_mesh_center': True,       # [False]
+        'force_cut_selection': ['high'],        # []
+        'make_cylindrical_pcb': True,           # [False]
+        'conductor_cross_section_width': 0.015,  # [0.002]
         'cross_sectional_points': np.array([np.sin(np.linspace(0, 2 * np.pi, 10)),
-                                     np.cos(np.linspace(0, 2 * np.pi, 10))]) * 0.01,
-        'sf_opt_method': 'tikhonov', # ...
-        'tikhonov_reg_factor': 100,  # %Tikhonov regularization factor for the SF optimization
+                                            np.cos(np.linspace(0, 2 * np.pi, 10))]) * 0.01,
+        'tikhonov_reg_factor': 100,             # Tikhonov regularization factor for the SF optimization [1]
 
-        'output_directory': 'images',
-        'project_name': 'ygradient_coil',
-        'fasthenry_bin': '../FastHenry2/bin/fasthenry',
-        'persistence_dir': 'debug',
-        #'debug': DEBUG_VERBOSE,
-        'debug': DEBUG_BASIC,
+        'output_directory': 'images',           # [Current directory]
+        'project_name': 'ygradient_coil',       # ['CoilGen']
+        'fasthenry_bin': '../FastHenry2/bin/fasthenry',  # [/usr/bin/fasthenry']
+        'persistence_dir': 'debug',             # [debug]
+        # 'debug': DEBUG_VERBOSE,
+        'debug': DEBUG_BASIC,                   # [0 = NONE]
     }
 
     result = pyCoilGen(log, arg_dict)
