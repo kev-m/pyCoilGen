1bb2994ce650e9a647ce82566efeb8c6959ecdef
Kevin Meyer <kev-m@users.noreply.github.com>
2023-09-16 20:41:33 +0200

    Adding plot_3D_contours_with_sf

--- pyCoilGen/plotting/__init__.py
+++ pyCoilGen/plotting/__init__.py
@@ -1,3 +1,4 @@
 from .plot_error_different_solutions import plot_error_different_solutions
 from .plot_various_error_metrics import plot_various_error_metrics
 from .plot_2D_contours_with_sf import plot_2D_contours_with_sf
+from .plot_3D_contours_with_sf import plot_3D_contours_with_sf
