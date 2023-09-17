df173008e4001d0efb54dcfe8480b12f85d82e21
Kevin Meyer <kev-m@users.noreply.github.com>
2023-09-17 15:28:42 +0200

    Generalising the plots and labels

--- pyCoilGen/plotting/plot_vector_field.py
+++ pyCoilGen/plotting/plot_vector_field.py
@@ -6,55 +6,7 @@ import matplotlib.pyplot as plt
 from pyCoilGen.sub_functions.data_structures import CoilSolution, SolutionErrors, FieldErrors, TargetField
 
 
-def plot_vector_field_xy(coords: np.ndarray, field: np.ndarray, plot_title='plot_vector_field_xy', save_figure=False):
-    """
-    Generate a contour plot of the field vectors in the X-Z plane.
-
-    Args:
-        coords (np.ndarray): The field co-ordinates (3,n)
-        field (np.ndarray): The field vector (3,n)
-
-    Returns:
-        None
-    """
-    # Find the middle z-coordinate value
-    middle_z = np.median(coords[2])
-
-    # Define the extent of your data (adjust as needed)
-    x_min, x_max = np.min(coords[0]), np.max(coords[0])
-    y_min, y_max = np.min(coords[1]), np.max(coords[1])
-
-    # Create a grid of x and y coordinates
-    x, y = np.meshgrid(np.linspace(x_min, x_max, 100), np.linspace(y_min, y_max, 100))
-
-    # Evaluate the magnetic field strength at each point
-    z = np.full_like(x, middle_z)  # Set z to middle z-coordinate
-    b_field = np.zeros_like(x)
-
-    for i in range(len(x)):
-        for j in range(len(y)):
-            point = np.array([x[i, j], y[i, j], z[i, j]]).reshape(3, 1)
-            dist = np.linalg.norm(coords - point, axis=0)
-            weights = 1 / dist
-            weights /= np.sum(weights)
-            b_field[i, j] = np.sum(field * weights)
-
-    # Create a contour plot
-    plt.figure(figsize=(8, 6))
-    contours = plt.contourf(x, y, b_field, cmap='viridis')
-    plt.colorbar(contours)
-    plt.title('Contour Plot of Magnetic Field Strengths\n(X-Y Plane)')
-    plt.xlabel('X')
-    plt.ylabel('Y')
-
-    # Save the figure if specified
-    if save_figure:
-        plt.savefig(f'images/{plot_title}.png', dpi=75)
-    else:
-        plt.show()
-
-
-def plot_vector_field(coords: np.ndarray, field: np.ndarray, magic=[0, 1, 2], plot_title='plot_vector_field_', save_figure=False):
+def plot_vector_field(coords: np.ndarray, field: np.ndarray, magic=[0, 1, 2], plot_title='Vector Field ', save_figure=False):
     """
     Generate a contour plot of the field vectors in a plane.
 
@@ -69,9 +21,10 @@ def plot_vector_field(coords: np.ndarray, field: np.ndarray, magic=[0, 1, 2], pl
         None
     """
 
-    axis_chars = [str(chr(ord('x') + magic_value)) for magic_value in magic]
-    if plot_title[-1] == '_':
-        plot_title += axis_chars[2]
+    axis_chars = [str(chr(ord('X') + magic_value)) for magic_value in magic]
+    if plot_title[-1] == ' ':
+        plot_title += axis_chars[0]
+        plot_title += axis_chars[1]
     # Find the middle z-coordinate value
     middle_z = np.median(coords[magic[2]])
 
@@ -94,18 +47,18 @@ def plot_vector_field(coords: np.ndarray, field: np.ndarray, magic=[0, 1, 2], pl
     plt.figure(figsize=(8, 6))
     contours = plt.contourf(xs, ys, z_matrix, cmap='viridis')
     plt.colorbar(contours)
-    plt.title(f'Contour Plot of Magnetic Field Strengths\n({axis_chars[2].upper()} Plane = {round(middle_z,3)})')
-    plt.xlabel(axis_chars[0].upper())
-    plt.ylabel(axis_chars[1].upper())
+    plt.title(f'Plot of {plot_title}\n({axis_chars[2]} Plane = {round(middle_z,3)})')
+    plt.xlabel(axis_chars[0])
+    plt.ylabel(axis_chars[1])
 
     # Save the figure if specified
     if save_figure:
-        plt.savefig(f'images/{plot_title}.png', dpi=75)
+        plt.savefig(f'images/plot_{plot_title.replace(" ","_")}.png', dpi=75)
     else:
         plt.show()
 
 
-def plot_vector_field_xy(coords: np.ndarray, field: np.ndarray, plot_title='plot_vector_field_xy', save_figure=False):
+def plot_vector_field_xy(coords: np.ndarray, field: np.ndarray, plot_title='Vector Field XY', save_figure=False):
     """
     Generate a contour plot of the field vectors in the X-Y plane, centred on the Z-values.
 
@@ -121,7 +74,7 @@ def plot_vector_field_xy(coords: np.ndarray, field: np.ndarray, plot_title='plot
     return plot_vector_field(coords=coords, field=field, magic=[0, 1, 2], plot_title=plot_title, save_figure=save_figure)
 
 
-def plot_vector_field_yz(coords: np.ndarray, field: np.ndarray, plot_title='plot_vector_field_yz', save_figure=False):
+def plot_vector_field_yz(coords: np.ndarray, field: np.ndarray, plot_title='Vector Field YZ', save_figure=False):
     """
     Generate a contour plot of the field vectors in the Y-Z plane, centred on the X-values.
 
@@ -137,7 +90,7 @@ def plot_vector_field_yz(coords: np.ndarray, field: np.ndarray, plot_title='plot
     return plot_vector_field(coords=coords, field=field, magic=[1, 2, 0], plot_title=plot_title, save_figure=save_figure)
 
 
-def plot_vector_field_xz(coords: np.ndarray, field: np.ndarray, plot_title='plot_vector_field_xz', save_figure=False):
+def plot_vector_field_xz(coords: np.ndarray, field: np.ndarray, plot_title='Vector Field XZ', save_figure=False):
     """
     Generate a contour plot of the field vectors in the X-Z plane, centred on Y.
 
