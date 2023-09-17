e45e005a3f73799c72810649cfa7d8a9df8e7c0b
Kevin Meyer <kev-m@users.noreply.github.com>
2023-09-17 10:13:53 +0200

    Reformatting

--- pyCoilGen/plotting/plot_error_different_solutions.py
+++ pyCoilGen/plotting/plot_error_different_solutions.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 
 from pyCoilGen.sub_functions.data_structures import CoilSolution
 
+
 def plot_error_different_solutions(coil_solutions: List[CoilSolution], solutions_to_plot: List[int], plot_title: str, save_figure=False):
     """
     Plots error metrics for different coil solutions.
@@ -20,14 +21,22 @@ def plot_error_different_solutions(coil_solutions: List[CoilSolution], solutions
     Returns:
         None
     """
-    max_rel_error_layout_vs_target = np.array([coil_solutions[x].solution_errors.field_error_vals.max_rel_error_layout_vs_target for x in solutions_to_plot])
-    mean_rel_error_layout_vs_target = np.array([coil_solutions[x].solution_errors.field_error_vals.mean_rel_error_layout_vs_target for x in solutions_to_plot])
-    max_rel_error_loops_vs_target = np.array([coil_solutions[x].solution_errors.field_error_vals.max_rel_error_unconnected_contours_vs_target for x in solutions_to_plot])
-    mean_rel_error_loops_vs_target = np.array([coil_solutions[x].solution_errors.field_error_vals.mean_rel_error_unconnected_contours_vs_target for x in solutions_to_plot])
-    max_rel_error_layout_vs_sf = np.array([coil_solutions[x].solution_errors.field_error_vals.max_rel_error_layout_vs_stream_function_field for x in solutions_to_plot])
-    mean_rel_error_layout_vs_sf = np.array([coil_solutions[x].solution_errors.field_error_vals.mean_rel_error_layout_vs_stream_function_field for x in solutions_to_plot])
-    max_rel_error_loops_vs_sf = np.array([coil_solutions[x].solution_errors.field_error_vals.max_rel_error_unconnected_contours_vs_stream_function_field for x in solutions_to_plot])
-    mean_rel_error_loops_vs_sf = np.array([coil_solutions[x].solution_errors.field_error_vals.mean_rel_error_unconnected_contours_vs_stream_function_field for x in solutions_to_plot])
+    max_rel_error_layout_vs_target = np.array(
+        [coil_solutions[x].solution_errors.field_error_vals.max_rel_error_layout_vs_target for x in solutions_to_plot])
+    mean_rel_error_layout_vs_target = np.array(
+        [coil_solutions[x].solution_errors.field_error_vals.mean_rel_error_layout_vs_target for x in solutions_to_plot])
+    max_rel_error_loops_vs_target = np.array(
+        [coil_solutions[x].solution_errors.field_error_vals.max_rel_error_unconnected_contours_vs_target for x in solutions_to_plot])
+    mean_rel_error_loops_vs_target = np.array(
+        [coil_solutions[x].solution_errors.field_error_vals.mean_rel_error_unconnected_contours_vs_target for x in solutions_to_plot])
+    max_rel_error_layout_vs_sf = np.array(
+        [coil_solutions[x].solution_errors.field_error_vals.max_rel_error_layout_vs_stream_function_field for x in solutions_to_plot])
+    mean_rel_error_layout_vs_sf = np.array(
+        [coil_solutions[x].solution_errors.field_error_vals.mean_rel_error_layout_vs_stream_function_field for x in solutions_to_plot])
+    max_rel_error_loops_vs_sf = np.array(
+        [coil_solutions[x].solution_errors.field_error_vals.max_rel_error_unconnected_contours_vs_stream_function_field for x in solutions_to_plot])
+    mean_rel_error_loops_vs_sf = np.array(
+        [coil_solutions[x].solution_errors.field_error_vals.mean_rel_error_unconnected_contours_vs_stream_function_field for x in solutions_to_plot])
 
     plt.figure(figsize=(10, 6))
 
@@ -37,10 +46,11 @@ def plot_error_different_solutions(coil_solutions: List[CoilSolution], solutions
     p4 = plt.plot(solutions_to_plot, mean_rel_error_layout_vs_sf, '*-r', linewidth=2)
     p5 = plt.plot(solutions_to_plot, max_rel_error_loops_vs_target, 'o-y', linewidth=2)
     p6 = plt.plot(solutions_to_plot, max_rel_error_layout_vs_target, 'o-m', linewidth=2)
-    p7 = plt.plot(solutions_to_plot, mean_rel_error_loops_vs_target, '*-', linewidth=2, color=[0, 0.5, 0], markerfacecolor=[0, 0.5, 0])
+    p7 = plt.plot(solutions_to_plot, mean_rel_error_loops_vs_target, '*-',
+                  linewidth=2, color=[0, 0.5, 0], markerfacecolor=[0, 0.5, 0])
     p8 = plt.plot(solutions_to_plot, mean_rel_error_layout_vs_target, '*-c', linewidth=2)
 
-    plt.legend(['max_rel_error_loops_vs_sf', 'max_rel_error_layout_vs_sf', 'mean_rel_error_loops_vs_sf', 'mean_rel_error_layout_vs_sf', 
+    plt.legend(['max_rel_error_loops_vs_sf', 'max_rel_error_layout_vs_sf', 'mean_rel_error_loops_vs_sf', 'mean_rel_error_layout_vs_sf',
                 'max_rel_error_loops_vs_target', 'max_rel_error_layout_vs_target', 'mean_rel_error_loops_vs_target', 'mean_rel_error_layout_vs_target'])
     plt.xlabel('solutions_to_plot')
     plt.ylabel('Error Values')
--- pyCoilGen/plotting/plot_various_error_metrics.py
+++ pyCoilGen/plotting/plot_various_error_metrics.py
@@ -1,4 +1,4 @@
-import logging 
+import logging
 import numpy as np
 from typing import List
 
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 
 from pyCoilGen.sub_functions.data_structures import CoilSolution, SolutionErrors, FieldErrors
 
-def plot_various_error_metrics(coil_layouts: List[CoilSolution], single_ind_to_plot : int, plot_title : str, save_figure=False):
+
+def plot_various_error_metrics(coil_layouts: List[CoilSolution], single_ind_to_plot: int, plot_title: str, save_figure=False):
     """
     Plots various error metrics for a specific coil layout.
 
@@ -24,10 +25,10 @@ def plot_various_error_metrics(coil_layouts: List[CoilSolution], single_ind_to_p
     dot_size = 200
 
     coil_solution: CoilSolution = coil_layouts[single_ind_to_plot]
-    errors : SolutionErrors = coil_solution.solution_errors
+    errors: SolutionErrors = coil_solution.solution_errors
 
     layout_c = errors.combined_field_layout[2]              # field_by_layout (3,257)
-    sf_c = coil_solution.sf_b_field[:,2]                    # b_field_opt_sf (257,3)
+    sf_c = coil_solution.sf_b_field[:, 2]                    # b_field_opt_sf (257,3)
     loops_c = errors.combined_field_loops[2]                # field_by_unconnected_loops (3,257)
     target_c = coil_solution.target_field.b[2]              # target_field.b (3,257)
     # loops_c_1A = errors.combined_field_loops_per1Amp[2]     # field_loops_per1Amp (3,257)
@@ -56,8 +57,10 @@ def plot_various_error_metrics(coil_layouts: List[CoilSolution], single_ind_to_p
         (ax5, abs(sf_c - target_c) / np.max(np.abs(target_c)) * 100, 'Relative SF error, [%]'),
         (ax6, abs(layout_c - target_c) / np.max(np.abs(target_c)) * 100, 'Relative error\n layout vs. target, [%]'),
         (ax7, abs(layout_c - sf_c) / np.max(np.abs(sf_c)) * 100, 'Relative error\n layout vs. sf field, [%]'),
-        (ax8, abs(loops_c - target_c) / np.max(np.abs(target_c)) * 100, 'Relative error\n unconnected contours vs. target, [%]'),
-        (ax9, abs(loops_c - layout_c) / np.max(np.abs(target_c)) * 100, 'Field difference between unconnected contours\n and final layout, [%]')
+        (ax8, abs(loops_c - target_c) / np.max(np.abs(target_c)) *
+         100, 'Relative error\n unconnected contours vs. target, [%]'),
+        (ax9, abs(loops_c - layout_c) / np.max(np.abs(target_c)) * 100,
+         'Field difference between unconnected contours\n and final layout, [%]')
     ]
 
     for ax, plot_color, title in plot_data:
@@ -66,7 +69,8 @@ def plot_various_error_metrics(coil_layouts: List[CoilSolution], single_ind_to_p
         ax.set_xlabel('x[m]')
         ax.set_ylabel('y[m]')
         ax.set_zlabel('z[m]')
-        fig.colorbar(ax.scatter(pos_data[0], pos_data[1], pos_data[2], c=plot_color, s=dot_size, cmap='viridis'), ax=ax, label='Error %', pad=0.10)
+        fig.colorbar(ax.scatter(pos_data[0], pos_data[1], pos_data[2], c=plot_color,
+                     s=dot_size, cmap='viridis'), ax=ax, label='Error %', pad=0.10)
 
     plt.tight_layout(rect=[0, 0, 1, 0.95])
     if save_figure:
