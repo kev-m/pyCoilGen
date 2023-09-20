8b239b6c7c500b980b993be52490c87fa1dcaa1e
Kevin Meyer <kev-m@users.noreply.github.com>
2023-09-20 13:45:50 +0200

    Adding comment with default value

--- examples/preoptimzed_svd_coil.py
+++ examples/preoptimzed_svd_coil.py
@@ -50,7 +50,7 @@ if __name__ == '__main__':
         'project_name': 'Preoptimzed_SVD_Coil',
         'fasthenry_bin': '../FastHenry2/bin/fasthenry',
         'persistence_dir': 'debug',
-        'output_directory': 'images',
+        'output_directory': 'images', # [Current directory]
         'debug': DEBUG_BASIC,
     }
 
