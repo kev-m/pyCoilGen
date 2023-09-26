7d8d535bee340bc37e25382e9e3ee040b6720e7b
Kevin Meyer <kev-m@users.noreply.github.com>
2023-09-26 09:48:33 +0200

    fix: Making pyCoilGen callable from Linux CLI.

--- pyCoilGen/__main__.py
+++ pyCoilGen/__main__.py
@@ -4,10 +4,12 @@ import logging
 
 from pyCoilGen.pyCoilGen_release import pyCoilGen
 
-if __name__ == "__main__":
+def main():
     # Set up logging
     log = logging.getLogger(__name__)
     logging.basicConfig(level=logging.INFO)
-
     # Fetch parameters from the command-line
     pyCoilGen(log)
+
+if __name__ == "__main__":
+    main()
