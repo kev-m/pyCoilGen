b46e76f7ffa2d28132593dd72cf228dc30dd5be4
Kevin Meyer <kev-m@users.noreply.github.com>
2023-10-03 08:51:30 +0200

    Rename load_plugins to load_mesh_factory_plugins

--- tests/test_mesh_factory.py
+++ tests/test_mesh_factory.py
@@ -2,11 +2,12 @@
 from pyCoilGen.sub_functions.data_structures import DataStructure
 
 # Function under test
-from pyCoilGen.mesh_factory import load_plugins as load_mesh_factory_plugins
+from pyCoilGen.mesh_factory import load_mesh_factory_plugins
+
 
 def test_build_planar_mesh():
     input_args = DataStructure(planar_mesh_parameter_list=[0.25, 0.25, 20, 20, 1, 0, 0, 0, 0, 0, 0])
-    plugin_name = 'create planar mesh'.replace(' ', '_').replace('-','_')
+    plugin_name = 'create planar mesh'.replace(' ', '_').replace('-', '_')
     plugins = load_mesh_factory_plugins()
     found = False
     for plugin in plugins:
@@ -17,5 +18,3 @@ def test_build_planar_mesh():
             break
 
     assert found
-
-test_build_planar_mesh()
