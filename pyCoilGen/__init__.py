8b8c45a39a85c03157b5d5213916dbe16c5f661f
Kevin Meyer <kev-m@users.noreply.github.com>
2023-10-09 11:30:06 +0200

    Bumping version

--- pyCoilGen/__init__.py
+++ pyCoilGen/__init__.py
@@ -1,3 +1,3 @@
 """Magnetic Field Coil Generator for Python."""
 # Semantic Versioning according to https://semver.org/spec/v2.0.0.html
-__version__ = "0.1.1" # Next: 0.1.1 Due to bugfix in build_biplanar_mesh.py
+__version__ = "0.1.2" # Next: 0.1.2 Due to generalising dependencies for pip
