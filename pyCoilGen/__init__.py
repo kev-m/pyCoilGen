3d5c6959d5ac3cd3e2bfae8218f35e4f748e8a58
Kevin Meyer <kev-m@users.noreply.github.com>
2023-11-07 19:22:56 +0100

    Updating next version

--- pyCoilGen/__init__.py
+++ pyCoilGen/__init__.py
@@ -1,3 +1,3 @@
 """Magnetic Field Coil Generator for Python."""
 # Semantic Versioning according to https://semver.org/spec/v2.0.0.html
-__version__ = "0.1.3" # Patch level, due to bugfixes from CoilGen. Issue #60
+__version__ = "0.2.0" # Patch level, due to bugfixes from CoilGen. Issue #60
